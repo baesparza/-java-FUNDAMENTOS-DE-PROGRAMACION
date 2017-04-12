df96a55cc386c2208da4ade3ad0e9b9994cf34ca
baesparza <bruno.be81@gmail.com>
2017-04-12 15:46:37 -0500

    first commit

---
+++ Estadistica/src/estadistica/Estadistica.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package estadistica;
+import java.util.Scanner;
+
+/**
+ * @baesparza
+ */
+public class Estadistica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner estatura_input= new Scanner(System.in);   
+        
+        //Variables
+        double persona1;
+        double persona2;
+        double persona3;
+        double persona4;
+        double persona5;
+        
+        double estatura;
+        double e_media;
+        
+        //Ingreso de datos
+        System.out.print("Ingrese la estatura de la persona 1: ");
+        persona1 = estatura_input.nextDouble();
+        System.out.print("Ingrese la estatura de la persona 2: ");
+        persona2 = estatura_input.nextDouble();
+        System.out.print("Ingrese la estatura de la persona 3: ");
+        persona3 = estatura_input.nextDouble();
+        System.out.print("Ingrese la estatura de la persona 4: ");
+        persona4 = estatura_input.nextDouble();
+        System.out.print("Ingrese la estatura de la persona 5: ");
+        persona5 = estatura_input.nextDouble();
+        
+        //Calcular media a partir de la duma de todas las estaturas
+        estatura = persona1 + persona2 + persona3 + persona4 + persona5;
+        e_media = estatura / 5;
+        
+        //Salida 
+        System.out.println("la estatura total de todas las persona es de: "+estatura);
+        System.out.println("la estatura media es de: "+e_media);
+        
+        
+    }
+    
+}
---
+++ Terreno/src/terreno/Terreno.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package terreno;
+import java.util.Scanner;
+
+/**
+ * @baesparza
+ */
+public class Terreno {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Scanner input= new Scanner(System.in);  
+        
+        //Variables 
+        double largo;
+        double ancho;
+        double costo;
+        
+        double total;
+        
+        //Ingreso de datos 
+        System.out.print("Ingrese el ancho del terreno: ");
+        ancho = input.nextDouble();
+        System.out.print("Ingrese el largo del terreno: ");
+        largo = input.nextDouble();
+        System.out.print("Ingrese el costo por metro cuadrado: ");
+        costo = input.nextDouble();
+        
+        //Calcular Costo 
+        total = (largo * ancho)* costo;
+        
+        //Salida 
+        System.out.print("El costo del terreno es "+total);
+        
+    }
+    
+}
